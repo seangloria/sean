764008d7ac955e3a3657fabd72f7c4b0d27edfb8
sean <sean@easyweddings.com.au>
2014-04-02 11:00:45 +1100

    te

--- app2/App_Start/BundleConfig.cs
+++ app2/App_Start/BundleConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
-namespace MvcApplication1
+namespace app2
 {
     public class BundleConfig
     {
--- app2/App_Start/WebApiConfig.cs
+++ app2/App_Start/WebApiConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
-namespace MvcApplication1
+namespace app2
 {
     public static class WebApiConfig
     {
